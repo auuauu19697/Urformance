0c052c252196d6365a4c14f80bc5b6e61b56cafb
auuauu19697 <au.pisit33@gmail.com>
2026-04-20 23:41:50 +0700

    chore(backend): ignore dist folder

--- backend/dist/drive/drive.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class DriveService {
-    private readonly config;
-    private readonly logger;
-    constructor(config: ConfigService);
-    private getDriveClient;
-    uploadSlip(file: Express.Multer.File, orderId: string): Promise<string>;
-}
