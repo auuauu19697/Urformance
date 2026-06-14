6d580c2c4695c4116075138f90f67df04eae797b
auuauu19697 <au.pisit33@gmail.com>
2026-06-15 00:56:40 +0700

    feat: ku product

--- frontend/src/themes/kutee-club/products.js
+++ frontend/src/themes/kutee-club/products.js
@@ -25,7 +25,55 @@ const products = [
       startTime: '2026-06-13T23:59:59+07:00',
       endTime: '2026-06-31T23:59:59+07:00',
     },
-  }
+  },
+  {
+    id: '2',
+    name: 'Ku Running Society 2',
+    tagline: 'Kasetsart 1943 Running Society 2',
+    price: 389,
+    pricingTiers: [
+      { minQty: 1, price: 389 },
+    ],
+    collectionImage: null,
+    colorImages: {
+      Black: null,
+      Cream: null,
+    },
+    colors: ['Black', 'Cream'],
+    sizes: ['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL'],
+    oversizeSurcharge: 20,
+    oversizeFrom: '3XL',
+    sizeChartImage: '/size-chart-default.png',
+    screeningFields: [],
+    orderWindow: {
+      startTime: '2026-06-13T23:59:59+07:00',
+      endTime: '2026-06-31T23:59:59+07:00',
+    },
+  },
+  {
+    id: '3',
+    name: 'Ku Polo',
+    tagline: 'Kasetsart 1943 Polo Shirt',
+    price: 389,
+    pricingTiers: [
+      { minQty: 1, price: 389 },
+    ],
+    collectionImage: null,
+    colorImages: {
+      Cream: null,
+      Pink: null,
+    },
+    colors: ['Cream', 'Pink'],
+    sizes: ['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL'],
+    oversizeSurcharge: 20,
+    oversizeFrom: '3XL',
+    sizeChartImage: '/size-chart-default.png',
+    screeningFields: [],
+    orderWindow: {
+      startTime: '2026-06-13T23:59:59+07:00',
+      endTime: '2026-06-31T23:59:59+07:00',
+    },
+  },
 ]
 
 export default products
